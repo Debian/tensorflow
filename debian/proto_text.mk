da76aa0569a53b42a5279053b1fa377f36cfeb50
Mo Zhou <cdluminate@gmail.com>
2018-09-01 08:20:00 +0000

    add proto_text.mk and bazelQuery.sh

---
+++ debian/proto_text.mk
@@ -0,0 +1,51 @@
+# TensorFlow Makefile for building proto_text
+
+# proto_text_cc_files.txt
+# proto_text_pb_cc_files.txt
+# proto_text_pb_h_files.txt
+
+BDIR := $(shell pwd)/proto_text.build/
+$(shell mkdir -p $(BDIR))
+PROTO_TEXT := $(BDIR)/proto_text
+PROTOC := protoc
+INCLUDES := -I.
+CXXFLAGS := -pthread -DPLATFORM_POSIX
+LIBS := -lpthread -lprotobuf
+
+.PHONY: all
+all: $(PROTO_TEXT)
+
+$(BDIR)%.o: %.cc
+	@mkdir -p $(dir $@)
+	$(CXX) -c $< -o $@ $(CPPFLAGS) $(CXXFLAGS) $(INCLUDES)
+
+$(BDIR)%.pb.o: $(BDIR)%.pb.cc
+	@mkdir -p $(dir $@)
+	$(CXX) -c $< -o $@ $(CPPFLAGS) $(CXXFLAGS) $(INCLUDES)
+
+$(BDIR)%.pb.cc $(BDIR)%.pb.h: %.proto
+	echo $<
+	@mkdir -p $(dir $@)
+	$(PROTOC) $< --cpp_out $(BDIR)
+
+PROTO_TEXT_CC_SRCS := $(shell cat proto_text_cc_files.txt)
+PROTO_TEXT_CC_OBJS := $(addprefix $(BDIR), $(PROTO_TEXT_CC_SRCS:.cc=.o))
+
+PROTO_TEXT_PB_CC_SRCS := $(shell cat proto_text_pb_cc_files.txt)
+PROTO_TEXT_PB_CC_OBJS := $(addprefix $(BDIR), $(PROTO_TEXT_PB_CC_SRCS:.cc=.o))
+
+PROTO_TEXT_PB_H_SRCS := $(shell cat proto_text_pb_h_files.txt)
+PROTO_TEXT_PB_H_GEN  := $(addprefix $(BDIR), $(PROTO_TEXT_PB_H_SRCS))
+
+PROTO_TEXT_OBJS := $(PROTO_TEXT_PB_CC_OBJS) $(PROTO_TEXT_CC_OBJS)
+
+$(PROTO_TEXT_OBJS): $(PROTO_TEXT_PB_H_GEN)
+
+$(PROTO_TEXT):  $(PROTO_TEXT_PB_H_GEN) $(PROTO_TEXT_OBJS)
+	@mkdir -p $(dir $@)
+	$(CXX) -o $(PROTO_TEXT) $(PROTO_TEXT_OBJS) \
+		$(CPPFLAGS) $(CXXFLAGS) $(LDFLAGS) $(INCLUDES) $(LIBS)
+
+.PHONY: clean
+clean:
+	-$(RM) -rf $(BDIR)
