ed64f46059bd96f8ec00ef057fe268214d964fe4
Mo Zhou <cdluminate@gmail.com>
2018-09-01 14:54:17 +0000

    native make: merge tf_core_* into a single tf_core.mk

---
+++ debian/tf_core.mk
@@ -0,0 +1,58 @@
+# Build core/kernels and core:ops for TensorFlow
+
+include debian/flags.mk
+
+TF_CORE := $(BDIR)/tf_core.a
+$(TF_CORE): $(BDIR)/tf_core_ops.a $(BDIR)/tf_core_kernels.a \
+		$(BDIR)/tf_core_lib.a
+	true
+
+# core / ops ------------------------------------------------------------------
+
+CORE_OPS_SRCS_EXCL := $(wildcard tensorflow/core/ops/*_test.cc) \
+	$(wildcard tensorflow/core/ops/*_test_utils.cc)
+CORE_OPS_SRCS := $(wildcard tensorflow/core/ops/*.cc)
+
+CORE_OPS_SRCS := $(filter-out $(CORE_OPS_SRCS_EXCL), $(CORE_OPS_SRCS))
+
+CORE_OPS_OBJS := $(addprefix $(BDIR), $(CORE_OPS_SRCS:.cc=.o))
+
+$(BDIR)/tf_core_ops.a: $(CORE_OPS_OBJS)
+	ar rcs $@ $(CORE_OPS_OBJS)
+
+# core / kernels --------------------------------------------------------------
+
+CORE_KERNELS_SRCS_EXCL := $(wildcard tensorflow/core/kernels/*test*) \
+	tensorflow/core/kernels/sparse_tensor_dense_matmul_op.cc \
+	tensorflow/core/kernels/conv_grad_ops_3d.cc \
+	tensorflow/core/kernels/adjust_contrast_op.cc \
+	tensorflow/core/kernels/debug_ops.cc
+CORE_KERNELS_SRCS := $(wildcard tensorflow/core/kernels/*.cc) \
+	$(wildcard tensorflow/core/kernels/*main.cc)
+
+CORE_KERNELS_SRCS := $(filter-out $(CORE_KERNELS_SRCS_EXCL), $(CORE_KERNELS_SRCS))
+
+CORE_KERNELS_OBJS := $(addprefix $(BDIR), $(CORE_KERNELS_SRCS:.cc=.o))
+
+$(BDIR)/tf_core_kernels.a: $(CORE_KERNELS_OBJS)
+	ar rcs $@ $(CORE_KERNELS_OBJS)
+
+# core / lib ------------------------------------------------------------------
+
+CORE_LIB_SRCS_EXCL := $(wildcard tensorflow/core/lib/*/*test.cc)
+CORE_LIB_SRCS := $(wildcard tensorflow/core/lib/*/*.cc)
+
+CORE_LIB_SRCS := $(filter-out $(CORE_LIB_SRCS_EXCL), $(CORE_LIB_SRCS))
+
+CORE_LIB_OBJS := $(addprefix $(BDIR), $(CORE_LIB_SRCS:.cc=.o))
+
+$(BDIR)/tf_core_lib.a: $(CORE_LIB_OBJS)
+	ar rcs $@ $(CORE_LIB_OBJS)
+
+# generic rule for objects ----------------------------------------------------
+
+# XXX: adding a "-w" argument to let GCC shutup or you'll get a giant buildlog.
+$(BDIR)%.o: %.cc
+	@mkdir -p $(dir $@)
+	$(CXX) -c $< -o $@ $(CPPFLAGS) $(CXXFLAGS) $(INCLUDES) -w
+# Build core/kernels and core:ops for TensorFlow
