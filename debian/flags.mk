de9118ee27d97f751da1619a790ab28e113f9b1a
Mo Zhou <cdluminate@gmail.com>
2018-09-01 10:31:42 +0000

    native make: misc improvement

--- debian/flags.mk
+++ debian/flags.mk
@@ -4,7 +4,8 @@ BDIR := $(shell pwd)/build/
 $(shell mkdir -p $(BDIR))
 PROTO_TEXT := $(BDIR)/proto_text
 PROTOC := protoc
-INCLUDES := -I. -I$(BDIR)
+INCLUDES := -I. -I$(BDIR) -Idebian/embedded/eigenn -I/usr/include/gemmlowp \
+	-Ithird_party/eigen3
 CXXFLAGS := -pthread -DPLATFORM_POSIX -std=c++14 -fPIC
 LIBS := -lpthread -lprotobuf
 LDFLAGS := -Wl,--as-needed
--- debian/tf_knl_ops.mk
+++ debian/tf_knl_ops.mk
@@ -2,8 +2,15 @@
 
 include debian/flags.mk
 
+KERNELS_CC_SRCS_EXCL := $(wildcard tensorflow/core/kernels/*_test.cc) \
+	$(wildcard tensorflow/core/kernels/*_test_utils.cc)
+OPS_CC_SRCS_EXCL := $(wildcard tensorflow/core/ops/*_test.cc) \
+	$(wildcard tensorflow/core/ops/*_test_utils.cc)
 KERNELS_CC_SRCS := $(wildcard tensorflow/core/kernels/*.cc)
 OPS_CC_SRCS := $(wildcard tensorflow/core/ops/*.cc)
+
+KERNELS_CC_SRCS := $(filter-out $(KERNELS_CC_SRCS_EXCL), $(KERNELS_CC_SRCS))
+OPS_CC_SRCS := $(filter-out $(OPS_CC_SRCS_EXCL), $(OPS_CC_SRCS))
 
 TF_KNL_OPS_OBJS := $(addprefix $(BDIR), $(KERNELS_CC_SRCS:.cc=.o)) \
 	$(addprefix $(BDIR), $(OPS_CC_SRCS:.cc=.o))
@@ -11,6 +18,7 @@ TF_KNL_OPS_OBJS := $(addprefix $(BDIR), $(KERNELS_CC_SRCS:.cc=.o)) \
 $(BDIR)/tf_knl_ops.a: $(TF_KNL_OPS_OBJS)
 	ar rcs $@ $(TF_KNL_OPS_OBJS)
 
+# XXX: adding a "-w" argument to let GCC shutup or you'll get a giant buildlog.
 $(BDIR)%.o: %.cc
 	@mkdir -p $(dir $@)
-	$(CXX) -c $< -o $@ $(CPPFLAGS) $(CXXFLAGS) $(INCLUDES)
+	$(CXX) -c $< -o $@ $(CPPFLAGS) $(CXXFLAGS) $(INCLUDES) -w
