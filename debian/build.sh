f01e97acd42d7f9bde29525932a17e463cf10e5a
Mo Zhou <cdluminate@gmail.com>
2018-09-01 13:36:23 +0000

    native make: scaffold shell script

---
+++ debian/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -x
+JOBS=72
+sh debian/embedded.sh
+make -f debian/proto_text.mk -j$JOBS
+make -f debian/tf_proto_text.mk -j$JOBS
+make -f debian/tf_proto.mk -j$JOBS
+make -f debian/tf_core_ops.mk -j$JOBS
