9cbd43cb15ab8f4379ccfebcc0139c802406027f
Mo Zhou <cdluminate@gmail.com>
2018-09-02 08:48:05 +0000

    native make: add tf_shlib.mk

---
+++ debian/tf_shlib.mk
@@ -0,0 +1,15 @@
+include debian/flags.mk
+include debian/globs.mk
+
+X_TF_SHLIB: $(BDIR)/libtensorflow.so
+
+$(BDIR)/libtensorflow.so: \
+		$(TF_PROTO_OBJS) $(PBT_OBJS) \
+		$(CORE_OBJS) $(TF_CC_OBJS) $(TF_C_OBJS)
+	$(CXX) -shared -fPIC $(CXXFLAGS) $(LDFLAGS) $(INCLUDES) $(LIBS) \
+		$(TF_PROTO_OBJS) $(PBT_OBJS) \
+		$(CORE_OBJS) $(TF_CC_OBJS) $(TF_C_OBJS) \
+		-o $(BDIR)/libtensorflow.so.$(VERSION) \
+		-Wl,--soname=libtensorflow.so.$(SOVERSION)
+	ln -sr $(BDIR)/libtensorflow.so.$(VERSION) $(BDIR)/libtensorflow.so.$(SOVERSION)
+	ln -sr $(BDIR)/libtensorflow/so.$(VERSION) $(BDIR)/libtensorlfow.so
