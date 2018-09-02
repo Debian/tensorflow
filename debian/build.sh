d022b1cc0e1c09a0604a34977d0261eea65cfff7
Mo Zhou <cdluminate@gmail.com>
2018-09-02 10:18:47 +0000

    native make: fix build

--- debian/build.sh
+++ debian/build.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
+# Build with native makefile from a clean git repo.
 set -x
 JOBS=72
+sh debian/bazelQuery.sh
+echo "tensorflow/core/protobuf/eager_service.proto
+tensorflow/core/grappler/costs/op_performance_data.proto
+tensorflow/core/kernels/boosted_trees/boosted_trees.proto" >> tf_proto_files.txt
 sh debian/embedded.sh
 make -f debian/proto_text.mk -j$JOBS
-make -f debian/tf_proto_text.mk -j$JOBS
 make -f debian/tf_proto.mk -j$JOBS
 make -f debian/tf_core.mk -j$JOBS
+make -f debian/tf_cc_op_gen.mk -j$JOBS
+make -f debian/tf_cc.mk -j$JOBS
+make -f debian/tf_c.mk -j$JOBS
+make -f debian/tf_shlib.mk -j$JOBS
+make -f debian/tf_benchmark_model.mk -j$JOBS
--- debian/tf_shlib.mk
+++ debian/tf_shlib.mk
@@ -4,7 +4,12 @@ include debian/globs.mk
 X_TF_SHLIB: $(BDIR)/libtensorflow.so
 
 TF_CC_OPS_SRCS := $(wildcard $(BDIR)/tensorflow/cc/ops/*.cc)
+TF_CC_OPS_SRCS := $(filter-out $(BDIR)/tensorflow/cc/ops/debug_ops.cc, $(TF_CC_OPS_SRCS))
 TF_CC_OPS_OBJS := $(TF_CC_OPS_SRCS:.cc=.o)
+
+$(BDIR)%.o: $(BDIR)%.cc
+       @mkdir -p $(dir $@)
+       $(CXX) -c $< -o $@ $(CPPFLAGS) $(CXXFLAGS) $(INCLUDES) -w
 
 $(BDIR)/libtensorflow.so: \
 		$(TF_PROTO_OBJS) $(PBT_OBJS) \
@@ -17,7 +22,3 @@ $(BDIR)/libtensorflow.so: \
 		-Wl,--allow-multiple-definition
 	ln -sr $(BDIR)/libtensorflow.so.$(VERSION) $(BDIR)/libtensorflow.so.$(SOVERSION)
 	ln -sr $(BDIR)/libtensorflow.so.$(VERSION) $(BDIR)/libtensorflow.so
-
-$(BDIR)%.o: %.cc
-	@mkdir -p $(dir $@)
-	$(CXX) -c $< -o $@ $(CPPFLAGS) $(CXXFLAGS) $(INCLUDES) -w
