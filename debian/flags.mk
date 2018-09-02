955a603310fee545be9dfbe33e676c2120cf0790
Mo Zhou <cdluminate@gmail.com>
2018-09-02 08:30:56 +0000

    native make: flags.mk

--- debian/flags.mk
+++ debian/flags.mk
@@ -5,10 +5,10 @@ $(shell mkdir -p $(BDIR))
 PROTO_TEXT := $(BDIR)/proto_text
 PROTOC := protoc
 INCLUDES := -I/usr/include -I. -I$(BDIR) -Idebian/embedded/eigen -I/usr/include/gemmlowp \
-	-Ithird_party/eigen3 -I/usr/include/google
-CXXFLAGS := -pthread -DPLATFORM_POSIX -std=c++14 -fPIC
+	-Ithird_party/eigen3 -I/usr/include/google -I/usr/include/jsoncpp
+CXXFLAGS := -pthread -DPLATFORM_POSIX -std=c++14 -fPIC -gsplit-dwarf
 LIBS := -lpthread -lprotobuf -lnsync -lnsync_cpp -ldouble-conversion -ldl -lm -lz
-LDFLAGS := -Wl,--as-needed -Wl,--no-whole-archive
+LDFLAGS := -Wl,--as-needed
 
 TF_CORE := $(BDIR)/tf_core.a
 TF_CORE_LIB := $(BDIR)/tf_core_lib.a
