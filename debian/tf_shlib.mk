a6df11996b85f7754cd28212d6e86750fea7cadb
Mo Zhou <cdluminate@gmail.com>
2018-09-02 09:48:34 +0000

    native make: missing objects for shlib

--- debian/tf_shlib.mk
+++ debian/tf_shlib.mk
@@ -3,14 +3,21 @@ include debian/globs.mk
 
 X_TF_SHLIB: $(BDIR)/libtensorflow.so
 
+TF_CC_OPS_SRCS := $(wildcard $(BDIR)/tensorflow/cc/ops/*.cc)
+TF_CC_OPS_OBJS := $(TF_CC_OPS_SRCS:.cc=.o)
+
 $(BDIR)/libtensorflow.so: \
 		$(TF_PROTO_OBJS) $(PBT_OBJS) \
-		$(CORE_OBJS) $(TF_CC_OBJS) $(TF_C_OBJS)
+		$(CORE_OBJS) $(TF_CC_OBJS) $(TF_C_OBJS) $(TF_CC_OPS_OBJS)
 	$(CXX) -shared -fPIC $(CXXFLAGS) $(LDFLAGS) $(INCLUDES) $(LIBS) \
-		$(TF_PROTO_OBJS) $(PBT_OBJS) \
+		$(TF_PROTO_OBJS) $(PBT_OBJS) $(TF_CC_OPS_OBJS) \
 		$(CORE_OBJS) $(TF_CC_OBJS) $(TF_C_OBJS) \
 		-o $(BDIR)/libtensorflow.so.$(VERSION) \
 		-Wl,--soname=libtensorflow.so.$(SOVERSION) \
 		-Wl,--allow-multiple-definition
 	ln -sr $(BDIR)/libtensorflow.so.$(VERSION) $(BDIR)/libtensorflow.so.$(SOVERSION)
 	ln -sr $(BDIR)/libtensorflow.so.$(VERSION) $(BDIR)/libtensorflow.so
+
+$(BDIR)%.o: %.cc
+	@mkdir -p $(dir $@)
+	$(CXX) -c $< -o $@ $(CPPFLAGS) $(CXXFLAGS) $(INCLUDES) -w
