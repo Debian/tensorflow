0c884a6f7674ab674731a383c6fedccdfff22e9d
Mo Zhou <cdluminate@gmail.com>
2018-09-02 04:12:46 +0000

    native make: merge tf_proto.mk and tf_proto_text.mk

--- debian/tf_proto.mk
+++ debian/tf_proto.mk
@@ -5,6 +5,10 @@
 
 include debian/flags.mk
 
+X_TF_PROTO: $(BDIR)/tf_proto.a $(BDIR)/tf_proto_text.a
+
+# -----------------------------------------------------------------------------
+
 TF_PROTO := $(shell cat tf_proto_files.txt)
 TF_PROTO_H_GEN := $(addprefix $(BDIR), $(TF_PROTO:.proto=.pb.h))
 TF_PROTO_CC_GEN := $(addprefix $(BDIR), $(TF_PROTO:.proto=.pb.cc))
@@ -20,3 +24,23 @@ $(BDIR)%.pb.o: $(BDIR)%.pb.cc
 $(BDIR)%.pb.cc $(BDIR)%.pb.h: %.proto
 	@mkdir -p $(dir $@)
 	$(PROTOC) $< --cpp_out $(BDIR)
+
+# -----------------------------------------------------------------------------
+
+PBT_H := $(shell cat tf_pb_text_files.txt)
+PBT_H_GEN := $(addprefix $(BDIR), $(PBT_H))
+PBT_IMPL_GEN := $(addprefix $(BDIR), $(PBT_H:.pb_text.h=.pb_text-impl.h))
+PBT_CC_GEN := $(addprefix $(BDIR), $(PBT_H:.h=.cc))
+PBT_OBJS := $(addprefix $(BDIR), $(PBT_H:.h=.o))
+
+$(BDIR)/tf_proto_text.a: $(PBT_OBJS)
+	ar rcs $@ $(PBT_OBJS)
+
+$(BDIR)%.pb_text.o: $(BDIR)%.pb_text.cc
+	@mkdir -p $(dir $@)
+	$(CXX) -c $< -o $@ $(CPPFLAGS) $(CXXFLAGS) $(INCLUDES)
+
+$(BDIR)%.pb_text.cc $(BDIR)%.pb_text.h $(BDIR)%.pb_text-impl.h: %.proto | $(PROTO_TEXT)
+	@mkdir -p $(dir $@)
+	$(PROTO_TEXT) $(BDIR)/tensorflow/core tensorflow/core \
+		tensorflow/tools/proto_text/placeholder.txt $<
