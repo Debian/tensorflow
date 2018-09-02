b0c072b1b334b4d6a4ca784810da617da6c3c417
Mo Zhou <cdluminate@gmail.com>
2018-09-02 06:30:09 +0000

    native make: fix tf_cc_op_gen.mk

--- debian/tf_cc_op_gen.mk
+++ debian/tf_cc_op_gen.mk
@@ -9,4 +9,11 @@ TF_CC_OP_GEN_OBJS := $(addprefix $(BDIR), $(TF_CC_OP_GEN_SRCS:.cc=.o))
 test: $(TF_CC_OP_GEN_OBJS)
 	$(CXX) $(CXXFLAGS) $(INCLUDES) $(LIBS) \
 		build/tensorflow/core/ops/array_ops.o \
-		$(TF_CC_OP_GEN_OBJS) $(TF_CORE) -o test
+		$(TF_CC_OP_GEN_OBJS) $(TF_CORE) -o test \
+		$(BDIR)/tf_proto.a $(BDIR)/tf_proto_text.a \
+		tensorflow/core/lib/strings/proto_text_util.cc
+
+$(BDIR)%.o: %.cc
+	@mkdir -p $(dir $@)
+	$(CXX) -c $< -o $@ $(CPPFLAGS) $(CXXFLAGS) $(INCLUDES) -w
+
