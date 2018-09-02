f224ee739cbb6978d383eebe7034de0ae7e5b351
Mo Zhou <cdluminate@gmail.com>
2018-09-02 09:45:16 +0000

    native make: fix tf_shlib.mk

--- debian/tf_shlib.mk
+++ debian/tf_shlib.mk
@@ -10,6 +10,7 @@ $(BDIR)/libtensorflow.so: \
 		$(TF_PROTO_OBJS) $(PBT_OBJS) \
 		$(CORE_OBJS) $(TF_CC_OBJS) $(TF_C_OBJS) \
 		-o $(BDIR)/libtensorflow.so.$(VERSION) \
-		-Wl,--soname=libtensorflow.so.$(SOVERSION)
+		-Wl,--soname=libtensorflow.so.$(SOVERSION) \
+		-Wl,--allow-multiple-definition
 	ln -sr $(BDIR)/libtensorflow.so.$(VERSION) $(BDIR)/libtensorflow.so.$(SOVERSION)
 	ln -sr $(BDIR)/libtensorflow.so.$(VERSION) $(BDIR)/libtensorflow.so
