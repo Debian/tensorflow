446d02959d93bb433c680b2c55b66ffc7b5fb873
Mo Zhou <cdluminate@gmail.com>
2018-09-03 02:42:04 +0000

    ninja: import bazel dump for tf_core_proto_text

--- debian/ninja/bazelQuery.sh
+++ debian/ninja/bazelQuery.sh
@@ -32,20 +32,10 @@ bazel query 'kind("source file", deps(//tensorflow/tools/proto_text:gen_proto_te
 bazel query 'kind("generated file", deps(//tensorflow/tools/proto_text:gen_proto_text_functions))' \
 	> debian/ninja/tf_tool_proto_text.generated_file.txt
 
-bazel query 'kind("source file", deps(//tensorflow:libtensorflow.so))' \
-	> debian/ninja/tf_libtensorflow_so.source_file.txt
-bazel query 'kind("generated file", deps(//tensorflow:libtensorflow.so))' \
-	> debian/ninja/tf_libtensorflow_so.generated_file.txt
-
-bazel query 'kind("source file", deps(//tensorflow/tools/lib_package:libtensorflow_test))' \
-	> debian/ninja/tf_libtensorflow_test.source_file.txt
-bazel query 'kind("generated file", deps(//tensorflow/tools/lib_package:libtensorflow_test))' \
-	> debian/ninja/tf_libtensorflow_test.generated_file.txt
-
-bazel query 'kind("source file", deps(//tensorflow/python:pywrap_tensorflow))' \
-	> debian/ninja/tf_python_pywrap_tensorflow.source_file.txt
-bazel query 'kind("generated file", deps(//tensorflow/python:pywrap_tensorflow))' \
-	> debian/ninja/tf_python_pywrap_tensorflow.generated_file.txt
+bazel query 'kind("source file", deps(//tensorflow/core:proto_text))' \
+	> debian/ninja/tf_core_proto_text.source_file.txt
+bazel query 'kind("generated file", deps(//tensorflow/core:proto_text))' \
+	> debian/ninja/tf_core_proto_text.generated_file.txt
 
 bazel query 'kind("source file", deps(//tensorflow:libtensorflow_framework.so))' \
 	> debian/ninja/tf_libtensorflow_framework_so.source_file.txt
@@ -56,3 +46,18 @@ bazel query 'kind("source file", deps(//tensorflow/core:android_tensorflow_lib))
 	> debian/ninja/tf_core_android_tflib.source_file.txt
 bazel query 'kind("generated file", deps(//tensorflow/core:android_tensorflow_lib))' \
 	> debian/ninja/tf_core_android_tflib.generated_file.txt
+
+bazel query 'kind("source file", deps(//tensorflow/tools/lib_package:libtensorflow_test))' \
+	> debian/ninja/tf_libtensorflow_test.source_file.txt
+bazel query 'kind("generated file", deps(//tensorflow/tools/lib_package:libtensorflow_test))' \
+	> debian/ninja/tf_libtensorflow_test.generated_file.txt
+
+bazel query 'kind("source file", deps(//tensorflow:libtensorflow.so))' \
+	> debian/ninja/tf_libtensorflow_so.source_file.txt
+bazel query 'kind("generated file", deps(//tensorflow:libtensorflow.so))' \
+	> debian/ninja/tf_libtensorflow_so.generated_file.txt
+
+bazel query 'kind("source file", deps(//tensorflow/python:pywrap_tensorflow))' \
+	> debian/ninja/tf_python_pywrap_tensorflow.source_file.txt
+bazel query 'kind("generated file", deps(//tensorflow/python:pywrap_tensorflow))' \
+	> debian/ninja/tf_python_pywrap_tensorflow.generated_file.txt
