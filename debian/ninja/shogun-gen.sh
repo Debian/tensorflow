d2a837e0f8b61ff66719d9c2b97421823115e8ec
Mo Zhou <cdluminate@gmail.com>
2018-09-03 03:17:54 +0000

    ninja: add helper script shogun-gen.sh

---
+++ debian/ninja/shogun-gen.sh
@@ -0,0 +1,17 @@
+#!/usr/bin
+set -x
+SHOGUN=python3 shogun.py
+
+$SHOGUN ProtoText -i tf_tool_proto_text.source_file.txt
+
+$SHOGUN TFCoreProto -i tf_core_proto_text.source_file.txt \
+	-g tf_core_proto_text.generated_file.txt
+
+$SHOGUN TFFrame -i tf_libtensorflow_framework_so.source_file.txt \
+	-g tf_libtensorflow_framework_so.generated_file.txt
+
+$SHOGUN TFLibAndroid -i tf_core_android_tflib.source_file.txt \
+	-g tf_core_android_tflib.generated_file.txt
+
+#tf_libtensorflow_so.generated_file.txt
+#tf_libtensorflow_so.source_file.txt
