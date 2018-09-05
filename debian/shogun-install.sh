3415140d12233225ed5454cd1dfef0e1da7226c9
Mo Zhou <cdluminate@gmail.com>
2018-09-05 02:17:42 +0000

    shogun: the installation helper is really problematic

--- debian/shogun-install.sh
+++ debian/shogun-install.sh
@@ -1,40 +1,32 @@
 #!/bin/sh
-# Shogun's installation helper
-# Copyright (C) 2018 Mo Zhou <lumin@debian.org>, MIT/Expat license.
+# Shogun's problematic installation helper
 #set -x
 set -e
 
-dryrun=0
 destdir="debian/tmp/"
 version="1.10.1"
 soversion="1.10"
 
 # installation helper
+# usage: myinstall <srcfile> <dstfile>
+# XXX: Never do so: myinstall <srcfile> <dstdir>
 myinstall () {
 	# myinstall -m0644 src dest
 	if ! test -d $(dirname $3); then
-		if test $dryrun -eq 1; then
-			echo mkdir -p $(dirname $3);
-		else
-			mkdir -v -p $3;
-	   	fi
+		mkdir -v -p $(dirname $3);
 	fi
-	if test $dryrun -eq 1; then
-	   	echo install $1 $2 $3
-	else
-	   	install -v $1 $2 $3
-	fi
+	install -v $1 $2 $3
 }
 
 # install headers
 for hdr in $(cat libtensorflow.hdrs); do
-	myinstall -m0644 $hdr $destdir/usr/include/$(dirname $hdr)
+	myinstall -m0644 $hdr $destdir/usr/include/$hdr
 done
 
 # install libtensorflow_framework.so and libtensorflow.so
 for so in"libtensorflow_framework.so" "libtensorflow.so"; do
 	fpath="$destdir/usr/lib/$(dpkg-architecture -qDEB_HOST_MULTIARCH)/$so"
-	myinstall -m0644 $so $(dirname $fpath)
+	myinstall -m0644 $so $fpath
 	mv -v $fpath $fpath.$version
 	ln -vsr $fpath.$version $fpath.$soversion
 	ln -vsr $fpath.$version $fpath
