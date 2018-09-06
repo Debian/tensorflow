056834625a5bc5524f3e83e95fa1d8e246feadd9
Mo Zhou <cdluminate@gmail.com>
2018-09-06 13:52:35 +0000

    autopkgtest: add test tf_ex_label_image.sh

---
+++ debian/tests/tf_ex_label_image.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -xe
+
+incdir="-I. -Idebian/embedded/eigen3 -I/usr/include/tensorflow/eigen3"
+libs="-L. -ltensorflow_cc -lpthread -lprotobuf"
+cxx="g++"
+cppflags=""
+cxxflags="-w -O2 -fPIE -pie"
+ldflags=""
+
+$cxx $cppflags $cxxflags $ldflags $incdir $libs \
+	tensorflow/examples/label_image/main.cc \
+	-o tf_ex_label_image
+
+if ! test -r libtensorflow_cc.so.1.10; then
+	ln -sr libtensorflow_cc.so libtensorflow_cc.so.1.10
+fi
+
+# according to tensorflow/examples/label_image
+curl -L "https://storage.googleapis.com/download.tensorflow.org/models/inception_v3_2016_08_28_frozen.pb.tar.gz" |
+  tar -C tensorflow/examples/label_image/data -xz
+
+LD_LIBRARY_PATH=. ./tf_ex_label_image
+
+exit 0
+
