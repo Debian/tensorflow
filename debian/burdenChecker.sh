8a212752b4e521c27443afc68b88826ab0d798e5
Mo Zhou <cdluminate@gmail.com>
2019-10-02 00:16:00 +0800

    add the burdenChecker.sh

---
+++ debian/burdenChecker.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -e
+
+BURDENS=$(fdfind 'BUILD')
+
+for burden in $BURDENS; do
+	dirname=$(dirname $burden)
+	basename=$(basename $burden)
+	target=debian/buildsys/$dirname/meson.build
+
+	# any "contrib" stuff is not officially supported by TensorFlow
+	if $(echo $burden | grep 'contrib/' >/dev/null 2>/dev/null); then continue; fi
+
+	# we don't build these
+	if $(echo $burden | grep 'lite/' >/dev/null 2>/dev/null); then continue; fi
+	if $(echo $burden | grep 'java/' >/dev/null 2>/dev/null); then continue; fi
+
+	# we omit the distro-unfriendly stuff
+	if $(echo $burden | grep 'third_party/' >/dev/null 2>/dev/null); then continue; fi
+
+	if ! test -r $target; then
+		mkdir -p $(dirname $target)
+		cp -v $burden $target
+	fi
+done
