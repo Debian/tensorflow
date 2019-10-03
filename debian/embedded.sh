16bebc7623c52d2b7c5e98bd811d25e161a6fdd3
Mo Zhou <cdluminate@gmail.com>
2019-10-04 00:56:22 +0800

    eigen3

--- debian/embedded.sh
+++ debian/embedded.sh
@@ -4,6 +4,11 @@ if ! test -d external/com_google_absl/; then
   mkdir -p external/com_google_absl/
   tar xf debian/embedded/43ef2148c0936ebf7cb4be6b19927a9d9d145b8f.tar.gz -C external/com_google_absl/ --strip-components=1
 fi
+if ! test -d external/eigen3/; then
+  mkdir -p external/eigen3/
+  tar xf debian/embedded/049af2f56331.tar.gz -C external/eigen3 --strip-components=1
+  #cp -av third_party/eigen3 external/
+fi
 #ln -s . bazel-out
 #ln -s . k8-opt
 #ln -s . host
