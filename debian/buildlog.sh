0eae67474572c661b09495b928f5f60b3994769d
Mo Zhou <cdluminate@gmail.com>
2019-10-03 14:30:02 +0800

    add debian/buildlog.sh

---
+++ debian/buildlog.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+set -e
+# reference: https://git.archlinux.org/svntogit/community.git/tree/trunk/PKGBUILD?h=packages/tensorflow
+# reference: https://github.com/gentoo/gentoo/blob/master/sci-libs/tensorflow/tensorflow-2.0.0.ebuild
+
+export PYTHON_BIN_PATH=/usr/bin/python3
+export USE_DEFAULT_PYTHON_LIB_PATH=1
+export TF_NEED_JEMALLOC=0
+export TF_NEED_KAFKA=0
+export TF_NEED_OPENCL_SYCL=0
+export TF_NEED_AWS=0
+export TF_NEED_GCP=0
+export TF_NEED_HDFS=0
+export TF_NEED_S3=0
+export TF_ENABLE_XLA=0
+export TF_NEED_GDR=0
+export TF_NEED_VERBS=0
+export TF_NEED_OPENCL=0
+export TF_NEED_MPI=0
+export TF_NEED_TENSORRT=0
+export TF_NEED_NGRAPH=0
+export TF_NEED_IGNITE=0
+export TF_NEED_ROCM=0
+export TF_SET_ANDROID_WORKSPACE=0
+export TF_DOWNLOAD_CLANG=0
+export TF_IGNORE_MAX_BAZEL_VERSION=1
+
+echo > .tf_configure.bazelrc <<EOF
+build --action_env PYTHON_BIN_PATH="/usr/bin/python3"
+build --action_env PYTHON_LIB_PATH="/usr/lib/python3.7/dist-packages"
+build --python_path="/usr/bin/python3"
+build:xla --define with_xla_support=false
+build --config=xla
+build:opt --copt=-march=native
+build:opt --copt=-Wno-sign-compare
+build:opt --host_copt=-march=native
+build:opt --define with_default_optimizations=true
+test --flaky_test_attempts=3
+test --test_size_filters=small,medium
+test --test_tag_filters=-benchmark-test,-no_oss,-oss_serial
+test --build_tag_filters=-benchmark-test,-no_oss
+test --test_tag_filters=-gpu
+test --build_tag_filters=-gpu
+build --action_env TF_CONFIGURE_IOS="0"
+EOF
+
+bazel clean
+bazel build --config=v2 //tensorflow:libtensorflow_framework.so -s 2>&1 | tee debian/libtensorflow_framework.so.log
+bazel clean
+bazel build --config=v2 //tensorflow:libtensorflow.so -s 2>&1 | tee debian/libtensorflow.so.log
+bazel clean
+bazel build --config=v2 //tensorflow:libtensorflow_cc.so -s 2>&1 | tee debian/libtensorflow_cc.so.log
+bazel clean
