daf21d70103fb7a869cba6a128dc890059dd9a29
Mo Zhou <cdluminate@gmail.com>
2019-10-03 19:26:32 +0800

    update

--- debian/fakebazel.py
+++ debian/fakebazel.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # FakeBazel
 # Copyright (C) 2019 Mo Zhou <lumin@debian.org>
-import os, sys, re, argparse
+import os, sys, re, argparse, shlex
 from ninja_syntax import Writer
 from typing import *
 
@@ -10,8 +10,10 @@ class FakeBazel(object):
     def parseBuildlog(path: str) -> List[str]:
         cmdlines = []
         lines = open(path, 'rt').readlines()
-        states = [0] # (anther SUBCOMMAND)
+        states = [0, 0] # (anther SUBCOMMAND, bracket balance)
         for line in lines:
+            if line.startswith('#') and line.endswith(')'): continue
+            if line.startswith('WARNING:'): continue
             line = line.strip()
             if line.startswith('SUBCOMMAND'):
                 if states[0] == 1:
@@ -35,6 +37,25 @@ class FakeBazel(object):
                 "-D__TIMESTAMP__=.*?\s", '-D_FORTIFY_SOURCE=1']
         for r in res:
             line = re.sub(r, ' ', line)
+        #if line.startswith('/usr/lib/ccache/gcc'):
+        #    src, obj = [], ''
+        #    tokens = list(line.split())
+        #    for i, t in enumerate(tokens):
+        #        if t == '-o':
+        #            obj = tokens[i+1]
+        #    for i, t in enumerate(tokens):
+        #        if i == 0:
+        #            continue
+        #        if not t.startswith('-') and not t.endswith('.d'):
+        #            if t.endswith('.o') and re.match('.*\.so.*', obj):
+        #                src.append(t)
+        #            elif t.endswith('.o'):
+        #                pass
+        #            else:
+        #                src.append(t)
+        #    print(' [31;1mCC[m', src, '->', obj.replace('bazel-out/k8-opt/bin/', ''))
+        #else:
+        #    print(line)
         return ' '.join(line.split())
     @staticmethod
     def isUnwanted(line: str) -> bool:
@@ -42,9 +63,9 @@ class FakeBazel(object):
             '.*external/aws/aws-cpp-sdk-core/.*?.c[cp]?p?\s.*',
             '.*external/boringssl.*?.c[cp]?p?\s.*',
             '.*external/com_google_protobuf/.*?.c[cp]?p?\s.*',
-            '.*external/.*?.c\s',
-            '.*external/.*?.cc\s',
-            '.*external/.*?.cpp\s',
+            '.*external/\S*?.c\s',
+            '.*external/\S.*?.cc\s',
+            '.*external/\S*?.cpp\s',
             ]):
             return True
         else:
@@ -54,34 +75,86 @@ class FakeBazel(object):
         rinsed, count = [], 0
         for line in cmdlines:
             line = re.sub("'", '', line)
-            line = FakeBazel.stripCompilerArgs(line)
             if not FakeBazel.isUnwanted(line):
-                if line.startswith('/usr/lib/ccache/gcc'):
-                    src, obj = [], ''
-                    tokens = list(line.split())
-                    for i, t in enumerate(tokens):
-                        if t == '-o':
-                            obj = tokens[i+1]
-                    for i, t in enumerate(tokens):
-                        if i == 0:
-                            continue
-                        if not t.startswith('-') and not t.endswith('.d'):
-                            if t.endswith('.o') and re.match('.*\.so.*', obj):
-                                src.append(t)
-                            elif t.endswith('.o'):
-                                pass
-                            else:
-                                src.append(t)
-                    print(' [31;1mCC[m', src, '->', obj.replace('bazel-out/k8-opt/bin/', ''))
-                else:
-                    print(line)
                 rinsed.append(line)
             else:
                 count += 1
         print(f'* {count}/{len(cmdlines)} commands were drop out.')
         return rinsed
+    @staticmethod
+    def understandCmdlines(cmdlines: List[str]) -> (List, List):
+        '''
+        Understand and Rinse the command lines
+        '''
+        depgraph = []
+        for cmd in cmdlines:
+            if cmd.startswith('/usr/lib/ccache/gcc'):
+                # it's a CXX/LD command
+                target = {'type': 'CXX', 'src': [], 'obj': [], 'def': []}
+                tokens = shlex.split(cmd)
+                for (i,t) in enumerate(tokens[1:], 1):
+                    if re.match('-g\d', t):
+                        pass
+                    elif re.match('-c', t):
+                        pass
+                    elif re.match('-o', t):
+                        pass
+                    elif re.match('-O\d', t):
+                        pass
+                    elif re.match('-M\w', t):
+                        pass
+                    elif re.match('-U_FORTIFY_SOURCE', t):
+                        pass
+                    elif re.match("-D__TIME__=.*?", t):
+                        pass
+                    elif re.match("-D__TIMESTAMP__=.*?", t):
+                        pass
+                    elif re.match("-D__DATE__=.*?", t):
+                        pass
+                    elif re.match('-D_FORTIFY_SOURCE=1', t):
+                        pass
+                    elif re.match('-fstack-protect', t):
+                        pass
+                    elif re.match('-Wall', t):
+                        pass
+                    elif re.match('-Wunused-but-set-parameter', t):
+                        pass
+                    elif re.match('-Wno-free-nonheap-object', t):
+                        pass
+                    elif re.match('-Wno-shift-negative-value', t):
+                        pass
+                    elif re.match('-fno-omit-frame-pointer', t):
+                        pass
+                    elif re.match('-ffunction-sections', t):
+                        pass
+                    elif re.match('-fdata-sections', t):
+                        pass
+                    elif re.match('-fno-canonical-system-headers', t):
+                        pass
+                    elif re.match('-Wno-builtin-macro-redefined', t):
+                        pass
+                    elif re.match('-D\S+', t):
+                        target['def'].append(t)
+                    elif re.match('.*\.d$', t):
+                        pass
+                    elif re.match('-frandom-seed=.*', t):
+                        pass
+                    elif re.match('-iquote', t) or re.match('-iquote', tokens[i-1]):
+                        pass
+                    elif re.match('-isystem', t) or re.match('-isystem', tokens[i-1]):
+                        pass
+                    elif re.match('.*\.c[cp]?p?$', t):
+                        target['src'].append(t)
+                    elif re.match('-o', tokens[i-1]):
+                        target['obj'].append(t)
+                    else:
+                        raise Exception(f'what is {t}? prev={tokens[i-1]} next={tokens[i+1]}')
+                print(target)
+            else:
+                raise Exception(f"cannot understand: {cmd}")
+        return [], {}
     def __init__(self, path: str):
-        self.cmdlines = self.parseBuildlog(path)
-        self.cmdlines = self.rinseCmdlines(self.cmdlines)
+        cmdlines = self.parseBuildlog(path)
+        cmdlines, depgraph = self.understandCmdlines(cmdlines)
 
 fakeb = FakeBazel(sys.argv[1])
