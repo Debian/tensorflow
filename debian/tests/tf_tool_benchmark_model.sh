9ca474641b43f3e801244deae9ad67d9713043b7
Mo Zhou <cdluminate@gmail.com>
2019-10-25 21:00:14 +0800

    fix tests

--- debian/tests/tf_tool_benchmark_model.sh
+++ debian/tests/tf_tool_benchmark_model.sh
@@ -1,23 +1,25 @@
 #!/bin/sh
 set -xe
+export _CC_NORM=1
 
-incdir="-I. -Idebian/embedded/eigen3 -I/usr/include/tensorflow/eigen3"
-libs="-L. -ltensorflow_cc -lpthread -lprotobuf"
-cxx="g++"
-cppflags=""
-cxxflags="-w -O2 -fPIE -pie"
-ldflags=""
+src=(
+tensorflow/core/util/reporter.cc
+tensorflow/tools/benchmark/benchmark_model.cc
+tensorflow/tools/benchmark/benchmark_model_main.cc
+)
+elf=tf_benchmark_model.elf
+flags="-Wl,--allow-multiple-definition -Wl,--whole-archive -I."
+libs="-lpthread -ltensorflow_framework -ltensorflow  -lprotobuf "
+source debian/tests/_cc_test
 
-$cxx $cppflags $cxxflags $ldflags $incdir $libs \
-	tensorflow/core/util/reporter.cc \
-	tensorflow/tools/benchmark/benchmark_model.cc \
-	tensorflow/tools/benchmark/benchmark_model_main.cc \
-	-o tf_benchmark_model
+LD_LIBRARY_PATH=. ./tf_benchmark_model.elf || true
 
-if ! test -r libtensorflow_cc.so.2.0; then
-	ln -sr libtensorflow_cc.so libtensorflow_cc.so.2.0 || true
+if ! test -r inception5h.zip; then
+wget -c https://storage.googleapis.com/download.tensorflow.org/models/inception5h.zip
+unzip inception5h.zip -d /tmp/
 fi
-LD_LIBRARY_PATH=. ./tf_benchmark_model
+
+./tf_benchmark_model.elf --graph=/tmp/tensorflow_inception_graph.pb
 
 exit 0
 
