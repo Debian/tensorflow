f4ce6b1be10fc9b32571b5fe67ce0817ec8d099e
Mo Zhou <cdluminate@gmail.com>
2019-10-25 21:52:33 +0800

    update test script

--- debian/tests/tf_tool_benchmark_model.sh
+++ debian/tests/tf_tool_benchmark_model.sh
@@ -15,6 +15,13 @@ source debian/tests/_cc_test
 LD_LIBRARY_PATH=. ./tf_benchmark_model.elf || true
 
 if ! test -r inception5h.zip; then
+# See https://github.com/tensorflow/models/tree/master/research/slim#pre-trained-models
+wget -c http://download.tensorflow.org/models/resnet_v2_50_2017_04_14.tar.gz
+tar xvf resnet_v2_50_2017_04_14.tar.gz -C /tmp/
+wget -c http://download.tensorflow.org/models/inception_v4_2016_09_09.tar.gz
+tar xvf inception_v4_2016_09_09.tar.gz -C /tmp/
+wget -c https://storage.googleapis.com/mobilenet_v2/checkpoints/mobilenet_v2_1.4_224.tgz
+tar xvf mobilenet_v2_1.4_224.tgz -C /tmp/
 wget -c https://storage.googleapis.com/download.tensorflow.org/models/inception5h.zip
 unzip inception5h.zip -d /tmp/
 fi
